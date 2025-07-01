ccc6b1fb85c1ddaf3798f1db88ded101402d2680
PON PRASAD V <ponprasad28@gmail.com>
2025-07-01 15:19:24 +0530

    added enum for loginfo clientapp

---
+++ DataAccess/Enum/ClientApp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Enum
+{
+    public enum ClientApp
+    {
+        Revit = 1,
+        Web = 2
+    }
+}
--- DataAccess/Models/LogInfo.cs
+++ DataAccess/Models/LogInfo.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataAccess.Enum;
 
 namespace DataAccess.Models
 {
     public class LogInfo : UserActionBase
     {
         public DateTime LoginTime { get; set; } = DateTime.UtcNow;
+        public ClientApp LoginFrom { get; set; } = ClientApp.Revit;
 
     }
 }
